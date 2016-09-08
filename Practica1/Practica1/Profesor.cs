e6277dc7efdd38dfc39a0291d07590d0b559ff3c
Mike <mikeavt98@gmail.com>
2016-09-07 19:22:09 -0700

    Clases agregado

---
+++ Practica1/Practica1/Profesor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    public class Profesor
+    {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+        public int Sueldo { get; set; }
+        public string Materia { get; set; }
+    }
+}
--- Practica1/Practica1/Program.cs
+++ Practica1/Practica1/Program.cs
@@ -10,14 +10,19 @@ namespace Practica1
     {
         static void Main(string[] args)
         {
-            int x;
-            x = 100;
-            while (x >= 1)
-            {
-                Console.Write(x);
-                Console.Write(" - ");
-                x = x - 1;
-            }
+            Alumno _alumno = new Alumno();
+            Profesor _profesor = new Profesor();
+
+            _alumno.Edad = 18;
+            _alumno.Nombre = "Mike";
+            _alumno.NoControl = 170414;
+
+            _profesor.Edad = 23;
+            _profesor.Nombre = "Erick";
+            _profesor.Sueldo = 3000;
+
+            Console.WriteLine("La edad del alumno es: {0} su nombre es: {1} y su No. de Control es: {2}", _alumno.Edad, _alumno.Nombre, _alumno.NoControl);
+            Console.WriteLine("La edad del profesor es: {0} su nombre es: {1} y su sueldo es: {2}", _profesor.Edad, _profesor.Nombre, _profesor.Sueldo);
             Console.ReadKey();
         }
     }
