83cfe6c2f33042174837592c793d18b4b70d4c94
Mike <mikeavt98@gmail.com>
2016-09-07 18:44:12 -0700

    proyecto agregado

---
+++ Practica1/Practica1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 100;
+            while (x >= 1)
+            {
+                Console.Write(x);
+                Console.Write(" - ");
+                x = x - 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
